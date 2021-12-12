0355cab669639a148ba2481626082b15a48f8091
RayzikZ <bodnar200289@gmail.com>
2021-12-13 00:17:24 +0200

    Kyrsova1
    Kyrsova1

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Форма футбольна дитяча PlayGame Україна Чемпіонат світу 2018',
+    price: 209.99,
+    currency: 'UAH',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Чашка збірна України з футболу',
+    price: 139.99,
+    currency: 'UAH',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Дитячий боді збірної України з футболу',
+    price: 199.99,
+    currency: 'UAH',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Футболка форми збірної України 2021 Joma FED',
+    price: 1119.99,
+    currency: 'UAH',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Мяч для футболу Joma Ukraine White',
+    price: 599.99,
+    currency: 'UAH',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Толстовка збірної України з футболу',
+    price: 449.99,
+    currency: 'UAH',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
